dad013c2059d86b5bc22735f3adc0e680982dfaf
Phillip Moreno <Phillipmoreno123@gmail.com>
2026-03-13 22:27:14 -0400

    Update Styling for Contact Page

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import { Box, Card, Typography, TextField, Button, Link } from "@mui/material";
-import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import HandshakeOutlinedIcon from '@mui/icons-material/HandshakeOutlined';
 import { useMemo, useState } from "react";
 
 export default function Contact() {
@@ -30,7 +30,8 @@ export default function Contact() {
         flexDirection: "column",
         alignItems: "center",
         px: { xs: 2, sm: 3, md: 6 },
-        py: { xs: 3, md: 4 },
+        py: { xs: 6, md: 6 },
+        backgroundColor: "#fffafa",
       }}
     >
       <Box sx={{ textAlign: "center", mb: 2.5, maxWidth: 1100 }}>
@@ -45,7 +46,7 @@ export default function Contact() {
           <Typography
             component="h2"
             sx={{
-              fontSize: { xs: "2rem", sm: "2.35rem" },
+              fontSize: { xs: "1.5rem", sm: "2rem" },
               fontWeight: 900,
               lineHeight: 1.1,
               textDecoration: "underline",
@@ -54,25 +55,13 @@ export default function Contact() {
           >
             Let's Get In Touch
           </Typography>
-
-          <Link
-            href="#"
-            underline="none"
-            aria-label="Open link"
-            sx={{
-              display: "inline-flex",
-              alignItems: "center",
-              color: "text.primary",
-            }}
-          >
-            <OpenInNewIcon sx={{ fontSize: 26 }} />
-          </Link>
+          <HandshakeOutlinedIcon sx={{fontSize: "2rem"}}/>
         </Box>
 
         <Typography
           sx={{
             fontWeight: 600,
-            fontSize: { xs: "1rem", sm: "1.05rem" },
+            fontSize: { xs: "0.8rem", sm: "1rem" },
             color: "text.primary",
           }}
         >
@@ -87,10 +76,10 @@ export default function Contact() {
         sx={{
           width: "100%",
           maxWidth: 1500,
-          bgcolor: "#ffffff",
           borderRadius: 1.5,
           px: { xs: 1.5, sm: 2.5 },
           py: { xs: 1.75, sm: 2.25 },
+          backgroundColor: "#fffafa",
         }}
       >
         <Box
@@ -110,7 +99,8 @@ export default function Contact() {
               value={form.name}
               onChange={onChange("name")}
               fullWidth
-              InputProps={{ sx: { bgcolor: "#fff" } }}
+              size="small"
+              sx={{ bgcolor: "#fff"}}
             />
             <TextField
               placeholder="Email Address"
@@ -119,7 +109,8 @@ export default function Contact() {
               onChange={onChange("email")}
               fullWidth
               type="email"
-              InputProps={{ sx: { bgcolor: "#fff" } }}
+              size="small"
+              sx={{ bgcolor: "#fff"}}
             />
             <TextField
               placeholder="Subject"
@@ -127,7 +118,8 @@ export default function Contact() {
               value={form.subject}
               onChange={onChange("subject")}
               fullWidth
-              InputProps={{ sx: { bgcolor: "#fff" } }}
+              size="small"
+              sx={{ bgcolor: "#fff"}}
             />
             <TextField
               placeholder="Message"
@@ -136,12 +128,11 @@ export default function Contact() {
               onChange={onChange("message")}
               fullWidth
               multiline
-              minRows={12}
-              InputProps={{
-                sx: {
-                  bgcolor: "#fff",
-                  alignItems: "flex-start",
-                },
+              minRows={6}
+              size="small"
+              sx={{ 
+                bgcolor: "#fff",
+                alignItems: "flex-start"
               }}
             />
             <Button
