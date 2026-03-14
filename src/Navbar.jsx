b48d128e3b98a6a6771603bfbee364023e7cc0fd
Phillip Moreno <Phillipmoreno123@gmail.com>
2026-03-14 00:09:44 -0400

    renaming favicon file name

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import codeLogo from '../public/android-chrome-512x512.png'
+import codeLogo from '../public/favicon.png'
 
 export default function Navbar({scrollToSection, homeRef, aboutRef, projectRef, contactRef}) {
   return (
